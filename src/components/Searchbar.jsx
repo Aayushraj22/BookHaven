fcd890a52f4d002e62e263970ea69d5dcec7f149
Aayushraj22 <aygupta9334@gmail.com>
2025-01-07 17:45:55 +0530

    recent search recommendation features added in searchbar component, also its UI modified

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
-import { fetchData } from '../utility'
+import { fetchData, postData } from '../utility'
 import Button from './Button'
 import Fallback from '../utility/components/Fallback'
 
@@ -9,6 +9,9 @@ function Searchbar({closeSearchBox}) {
   const [searchText, setSearchText] = useState('')
   const [isloading, setIsloading] = useState(false)
   const [failedMsg, setFailedMsg] = useState('')
+  const [mostSearched, setMostSearched] = useState([])
+  const [isInputFocus, setIsInputFocus] = useState(false)
+
 
   const handleInputChange = (e) => {
     const inputText = e.target.value;
@@ -57,12 +60,32 @@ function Searchbar({closeSearchBox}) {
     })
     .finally(() => {
       setIsloading(false)
+      setIsInputFocus(false)
     })
+
+    postData(`rst?searchText=${searchText}`)
+  }
+
+  const handleRecommendSearchText = async() => {
+    // remove the failed message
+    setFailedMsg('')
+    setSearchResult([])
+
+    const endpoint = `rst`
+    fetchData(endpoint)
+    .then(data => {
+      setMostSearched([...data])
+      setIsInputFocus(true)
+    })
+  }
+
+  const handleInputFocusOut = () => {
+    setIsInputFocus(false)
   }
   
   return (
     <div className='fixed bg-transparent backdrop-blur-xl z-20 grid place-items-center left-0 right-0 top-0 bottom-0'>
-      <div className='w-5/6 md:w-4/6 h-4/5 md:h-4/6 text-stone-800 dark:text-stone-300 flex flex-col items-end p-3 rounded-md bg-stone-200 dark:bg-stone-700'>
+      <div className='w-5/6 md:w-1/2  text-stone-800 dark:text-stone-300 flex flex-col items-end p-3 rounded-md bg-stone-200 dark:bg-stone-700'>
         <Button
         margin={'mb-3'}
           bg={'bg-stone-300 dark:bg-stone-800'}
@@ -83,17 +106,22 @@ function Searchbar({closeSearchBox}) {
           value={searchText}
           onChange={handleInputChange}
           onKeyDown={handleEnterPress}
+          onFocus={handleRecommendSearchText}
+          onBlur={handleInputFocusOut}
           title='Search Book' 
         />
 
-        <section className={`p-4 w-11/12  rounded-md flex flex-col gap-1 overflow-y-auto h-full flex-1  mx-auto ${searchResult.length ? 'bg-stone-400 dark:bg-stone-900 text-stone-900 dark:text-stone-400' : 'bg-transparent'}`}>
+        <section className={` w-11/12  rounded-md flex flex-col gap-1 overflow-y-auto h-full flex-1 mx-auto ${searchResult.length || isInputFocus || failedMsg ? 'bg-stone-400 dark:bg-black text-stone-900 dark:text-stone-400 p-4 max-h-60' : 'bg-transparent'}`}>
           {isloading && (
             <Fallback loader={'magnifyingGlass'} />
           )}
-
+          {mostSearched?.length && !searchResult?.length && !isloading && isInputFocus ? (<>
+            <h3 className='text-sm font-sans capitalize italic'>Most frequent searches ... </h3>
+            {mostSearched?.map(obj => (<p key={obj._id} className='text-xs border dark:hover:bg-stone-950 hover:bg-stone-600 hover:text-stone-200 border-stone-500 hover:border-stone-200  p-2 capitalize rounded-md  cursor-pointer' onMouseDown={() => {setSearchText(obj?.text)}}>{obj?.text}</p>))}
+            </>) : ''}
           {searchResult?.map((book, index) => 
           <Link 
-            key={book?.id} 
+            key={book?._id} 
             to='/bookDetails' 
             state={book} 
             className='flex flex-col text-xs rounded p-2  relative shadow-sm shadow-stone-500 transition-all'
